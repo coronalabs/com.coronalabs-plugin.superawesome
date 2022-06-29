26669e51542f43f70aeed7e35e7e44757631f73e
Scott Harrison <scottrules44@gmail.com>
2022-06-28 23:56:08 -0400

    Fixed Typo in metadata

--- plugins/2019.3497/iphone-sim/metadata.lua
+++ plugins/2019.3497/iphone-sim/metadata.lua
@@ -6,10 +6,10 @@ local metadata =
 
 		-- This is the name without the 'lib' prefix.
 		-- In this case, the static library is called: libSTATIC_LIB_NAME.a
-		staticLibs = { "SuperAwesomePlugin", "Alamofire", "Moya", "SuperAwesome", "SwiftyXMLParser" }, 
+		staticLibs = { "SuperAwesomePlugin", },
 
-		frameworks = { "AdSupport" },
-		frameworksOptional = {},
+		frameworks = { "AdSupport", "WebKit", "CFNetwork", "Alamofire", "Moya", "SuperAwesome", "SwiftyXMLParser"},
+		frameworksOptional = {"HealthKit"},
 		usesSwift = true,
 	}
 }
